e82ddcbee2f2bcbb3e17ffb8085c15a2d1c7190f
bendalemahesh <bendalemahesh8@gmail.com>
2026-07-05 11:34:18 +0530

    updated

--- App/components/metrics.py
+++ App/components/metrics.py
@@ -5,7 +5,7 @@ def render_metrics():
 
     col1, col2, col3, col4 = st.columns(4)
 
-    col1.metric("Candidates", "156")
+    col1.metric("Candidates", "157")
     col2.metric("Shortlisted", "25")
     col3.metric("Average Score", "87%")
     col4.metric("Today's Screening", "12")
