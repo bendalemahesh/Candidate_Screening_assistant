4361e7e04930e6c67e00267fe971a20822c76bcf
bendalemahesh <bendalemahesh8@gmail.com>
2026-07-09 01:29:49 +0530

    Week 2: Added Resume Parser Agent and spaCy integration

--- App/models/gemini_model.py
+++ App/models/gemini_model.py
@@ -1,4 +1,5 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 
@@ -6,6 +7,6 @@ load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    google_api_key = st.secrets.get("GOOGLE_API_KEY", os.getenv("GOOGLE_API_KEY")),
     temperature=0,
 )
