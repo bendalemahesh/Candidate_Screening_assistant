28a315fc2fc9897900ff4c234a86c82b2a0b045d
bendalemahesh <bendalemahesh8@gmail.com>
2026-07-10 11:54:14 +0530

    changed gemini model to gemini-2.5-lite

--- App/models/gemini_model.py
+++ App/models/gemini_model.py
@@ -13,7 +13,7 @@ if not google_api_key:
         pass
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+    model="gemini-2.5-flash-lite",
     google_api_key=google_api_key,
     temperature=0,
 )
