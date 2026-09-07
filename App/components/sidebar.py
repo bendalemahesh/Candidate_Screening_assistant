39066cbe44e7b42f98bc1e7b0c9f0acc2fcd923b
bendalemahesh <bendalemahesh8@gmail.com>
2026-09-07 15:30:49 +0530

    backend Url connected

--- App/components/sidebar.py
+++ App/components/sidebar.py
@@ -4,7 +4,7 @@ import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
-BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8000")
+BACKEND_URL = os.getenv("BACKEND_URL", "https://candidate-screening-assistant.onrender.com")
 
 
 def render_sidebar():
