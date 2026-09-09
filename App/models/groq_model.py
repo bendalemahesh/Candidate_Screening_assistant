f5d1d6af0e9704e1055bea2f316f551f8b01600b
bendalemahesh <bendalemahesh8@gmail.com>
2026-09-09 15:47:11 +0530

    chenge llm model

--- App/models/groq_model.py
+++ App/models/groq_model.py
@@ -7,7 +7,7 @@ load_dotenv()
 groq_api_key = os.getenv("GROQ_API_KEY")
 
 llm = ChatGroq(
-    model="llama-3.1-8b-instant",
+    model="openai/gpt-oss-20b",
     groq_api_key=groq_api_key,
     temperature=0,
 )
